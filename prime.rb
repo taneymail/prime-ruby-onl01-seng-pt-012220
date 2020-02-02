5571a91dd7dd2d6662fa9a899a4294d57c923904
TShipley <taneymail@gmail.com>
2020-02-02 03:31:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
+def prime?(number)
+  if number < 2
+     return false
+  else 
+    (2..number - 1).to_a.all? do |n|
+      number % n != 0
+    end
+  end 
+end
+
+prime?(-1)
+
+
 # Add  code here!
